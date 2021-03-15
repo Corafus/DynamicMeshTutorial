9309a305a3894dff29af40a9c2df0d1afb829c3d
Corafus <36778688+Corafus@users.noreply.github.com>
2021-03-15 19:41:14 -0400

    Add files via upload

---
+++ band3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class band3 : MonoBehaviour
+{
+
+    public MeshFilter bandMesh;
+    public Vector3[] vertices;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vertices = new Vector3[bandMesh.mesh.vertices.Length];
+        for(var i = 0; i < vertices.Length; i++){
+          vertices[i] = bandMesh.mesh.vertices[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      bandMesh.mesh.vertices = vertices;
+    }
+}
