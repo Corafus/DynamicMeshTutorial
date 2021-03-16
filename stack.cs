d958dc7633836a4a5a08345704415c4dee0b3546
Corafus <36778688+Corafus@users.noreply.github.com>
2021-03-16 16:50:39 +0000

    Add files via upload

--- stack.cs
+++ stack.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class stack : MonoBehaviour
 {
     public MeshFilter stackMesh;
+    public float height;
     public Vector3[] vertices;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        vertices = new Vector3[stackMesh.mesh.vertices.Length];
-        for(var i = 0; i < vertices.Length; i++){
-          vertices[i] = stackMesh.mesh.vertices[i];
-        }
+      vertices = new Vector3[stackMesh.mesh.vertices.Length];
+      for(var i = 0; i < vertices.Length; i++){
+        vertices[i] = stackMesh.mesh.vertices[i];
+      }
+
+      height = 1.0f;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        stackMesh.mesh.vertices = vertices;
+
+      vertices[0].y = height;
+      vertices[1].y = height;
+      vertices[2].y = height;
+      vertices[3].y = height;
+
+      
+      stackMesh.mesh.vertices = vertices;
     }
 }
