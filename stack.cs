101bc1bbf1f1b8cf0e0cdca4683c43f03bce10bf
Corafus <36778688+Corafus@users.noreply.github.com>
2021-03-16 16:29:35 +0000

    Add files via upload

---
+++ stack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stack : MonoBehaviour
+{
+    public MeshFilter bandMesh;
+    public Vector3[] vertices;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vertices = new Vector3[bandMesh.mesh.vertices.Length];
+        for(var i = 0; i < vertices.Length; i++){
+          vertices[i] = bandMesh.mesh.vertices[i];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bandMesh.mesh.vertices = vertices;
+    }
+}
