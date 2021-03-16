fa5d61ba2e84049c61574464848db52692cfabc0
Corafus <36778688+Corafus@users.noreply.github.com>
2021-03-16 17:17:00 +0000

    Add files via upload

--- band.cs
+++ band.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class stack : MonoBehaviour
+public class band : MonoBehaviour
 {
     public MeshFilter bandMesh;
     public Vector3[] vertices;
+
+    public MeshFilter stackMesh;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,23 @@ public class stack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        int[] top = {10, 11, 9, 8, 17, 16, 23, 22};
+        for(var i = 0; i < top.Length; i++){
+          vertices[top[i]].y = stackMesh.mesh.vertices[0].y;
+        }
+
+        int[] topRight = {9, 8, 23, 22};
+        for(var i = 0; i < topRight.Length; i++){
+          vertices[topRight[i]].x = stackMesh.mesh.vertices[2].x;
+        }
+
+        int[] topLeft = {10, 11, 17, 16};
+        for(var i = 0; i < topLeft.Length; i++){
+          vertices[topLeft[i]].x = stackMesh.mesh.vertices[2].x;
+        }
+
+
         bandMesh.mesh.vertices = vertices;
     }
 }
