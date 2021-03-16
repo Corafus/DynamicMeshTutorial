9e9329a075cc872d67a4d2b3bc601318c67354dc
Corafus <36778688+Corafus@users.noreply.github.com>
2021-03-16 20:24:06 +0000

    Add files via upload

--- band.cs
+++ band.cs
@@ -39,6 +39,18 @@ public class band : MonoBehaviour
         for(var i = 0; i < bottomLeft.Length; i++){
           vertices[bottomLeft[i]].x = -1.6f - margin;
         }
+
+        int[] front = {10, 9, 22, 21, 14, 13, 0, 3, 6, 5, 18, 17};
+        for(var i = 0; i < front.Length; i++){
+          vertices[front[i]].z = -0.05f;
+        }
+
+        int[] back = {11, 8, 23, 20, 15, 12, 1, 2, 7, 4, 19, 16};
+        for(var i = 0; i < back.Length; i++){
+          vertices[back[i]].z = 0.05f;
+        }
+
+
     }
 
     // Update is called once per frame
@@ -57,7 +69,7 @@ public class band : MonoBehaviour
 
         int[] topLeft = {10, 11, 17, 16};
         for(var i = 0; i < topLeft.Length; i++){
-          vertices[topLeft[i]].x = stackMesh.mesh.vertices[1].x + margin;
+          vertices[topLeft[i]].x = stackMesh.mesh.vertices[1].x - margin;
         }
 
 
